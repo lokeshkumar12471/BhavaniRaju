3559cbf5409c513111925a34dee6b27d78ae85ca
lokesh <lokeshkumar12471@gmail.com>
2022-11-13 14:41:42 +0530

    Book Appointment Changes Was Done

--- resources/views/pages/frontend/bookappointment.blade.php
+++ resources/views/pages/frontend/bookappointment.blade.php
@@ -1,96 +1,120 @@
 <x-guest-layout>
-    <div class="container text-center mb-2">
-        <div class="flex items-center justify-center p-12 md:mt-24 mt-16"">
-            <!-- Author: FormBold Team -->
-            <!-- Learn More: https://formbold.com -->
-            <div class="mx-auto w-full max-w-[550px] bg-white">
-                <a href="{{ route('bookappointment') }}"
-                    class="btn bg-indigo-600  hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md mt-3">Book
-                    Appointment</a>
-                <p>Please Fill All The Fields</p>
-                <form action="#" method="POST">
-                    <div class="mb-5">
-                        <label for="name" class="mb-3 block text-base font-medium text-[#07074D]">
-                            Full Name
-                        </label>
-                        <input type="text" name="name" id="name" placeholder="Full Name"
-                            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                    </div>
-                    <div class="mb-5">
-                        <label for="phone" class="mb-3 block text-base font-medium text-[#07074D]">
-                            Phone Number
-                        </label>
-                        <input type="text" name="phone" id="phone" placeholder="Enter your phone number"
-                            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                    </div>
-                    <div class="mb-5">
-                        <label for="email" class="mb-3 block text-base font-medium text-[#07074D]">
-                            Email Address
-                        </label>
-                        <input type="email" name="email" id="email" placeholder="Enter your email"
-                            class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                    </div>
-                    <div class="-mx-3 flex flex-wrap">
-                        <div class="w-full px-3 sm:w-1/2">
-                            <div class="mb-5">
-                                <label for="date" class="mb-3 block text-base font-medium text-[#07074D]">
-                                    Date
-                                </label>
-                                <input type="date" name="date" id="date"
-                                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                            </div>
-                        </div>
-                        <div class="w-full px-3 sm:w-1/2">
-                            <div class="mb-5">
-                                <label for="time" class="mb-3 block text-base font-medium text-[#07074D]">
-                                    Time
-                                </label>
-                                <input type="time" name="time" id="time"
-                                    class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                            </div>
-                        </div>
-                    </div>
+    <div class="container md:mt-24 mt-16">
+        <div class="grid md:grid-cols-1 grid-cols-1 items-center gap-[30px]">
+            <div class="lg:col-span-5 md:col-span-6 mt-8 md:mt-0">
+                <div class="lg:ml-5">
+                    <h3 class="mb-6 text-2xl  font-medium text-center">Book Appointment</h3>
 
-                    <div class="mb-5 pt-3">
-                        <label class="mb-5 block text-base font-semibold text-[#07074D] sm:text-xl">
-                            Address Details
-                        </label>
+                    <form method="post" name="myForm" id="myForm" onsubmit="return validateForm()">
+                        <p class="mb-0" id="error-msg"></p>
+                        <div id="simple-msg"></div>
+                        <div class="grid lg:grid-cols-12 lg:gap-6">
+                            <div class="lg:col-span-12">
+                                <div class="text-left">
+                                    <label for="name" class="font-semibold">Full Name:</label>
+                                    <div class="form-icon relative mt-2">
+                                        <input name="name" id="name" type="text" class="form-input pl-2"
+                                            placeholder="Enter Your Full Name">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="lg:col-span-12 mb-5">
+                                <div class="text-left">
+                                    <label for="email" class="font-semibold"> Phone Number:</label>
+                                    <div class="form-icon relative mt-2">
+                                        <input name="email" id="email" type="email" class="form-input pl-2"
+                                            placeholder="Enter Your Phone Number">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid lg:grid-cols-12 lg:gap-6">
+                            <div class="lg:col-span-12 mb-5">
+                                <div class="text-left">
+                                    <label for="email" class="font-semibold">Email Address:</label>
+                                    <div class="form-icon relative mt-2">
+                                        <input name="email" id="email" type="email" class="form-input pl-2"
+                                            placeholder="Enter Your Email Address">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <div class="-mx-3 flex flex-wrap">
                             <div class="w-full px-3 sm:w-1/2">
-                                <div class="mb-5">
-                                    <input type="text" name="area" id="area" placeholder="Enter area"
-                                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                <div class="text-left">
+                                    <label for="email" class="font-semibold">Date:</label>
+                                    <div class="form-icon relative mt-2">
+                                        <input name="email" id="email" type="date" class="form-input pl-2"
+                                            placeholder="Enter Your Email Address">
+                                    </div>
                                 </div>
                             </div>
                             <div class="w-full px-3 sm:w-1/2">
-                                <div class="mb-5">
-                                    <input type="text" name="city" id="city" placeholder="Enter city"
-                                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                </div>
-                            </div>
-                            <div class="w-full px-3 sm:w-1/2">
-                                <div class="mb-5">
-                                    <input type="text" name="state" id="state" placeholder="Enter state"
-                                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
-                                </div>
-                            </div>
-                            <div class="w-full px-3 sm:w-1/2">
-                                <div class="mb-5">
-                                    <input type="text" name="post-code" id="post-code" placeholder="Post Code"
-                                        class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                                <div class="text-left">
+                                    <label for="email" class="font-semibold">Time:</label>
+                                    <div class="form-icon relative mt-2">
+                                        <input name="email" id="email" type="time" class="form-input pl-2"
+                                            placeholder="Enter Your Email Address">
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div>
-                        <button
-                            class="hover:shadow-form w-full rounded-md bg-indigo-600 py-3 px-8 text-center text-base font-semibold text-white outline-none">
-                            Book Appointment
-                        </button>
-                    </div>
-                </form>
+
+                        <div class="mb-5 pt-3">
+                            <div class="-mx-3 flex flex-wrap">
+                                <div class="w-full px-3 sm:w-1/2">
+                                    <div class="text-left">
+                                        <label for="email" class="font-semibold">Enter Your Area:</label>
+                                        <div class="form-icon relative mt-2">
+                                            <input name="email" id="email" type="area" class="form-input pl-2"
+                                                placeholder="Enter Your Area">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="w-full px-3 sm:w-1/2">
+                                    <div class="text-left">
+                                        <label for="email" class="font-semibold">Enter Your City:</label>
+                                        <div class="form-icon relative mt-2">
+                                            <input name="email" id="email" type="city" class="form-input pl-2"
+                                                placeholder="Enter Your City">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-5 pt-3">
+                            <div class="-mx-3 flex flex-wrap">
+                                <div class="w-full px-3 sm:w-1/2">
+                                    <div class="text-left">
+                                        <label for="email" class="font-semibold">Enter Your State</label>
+                                        <div class="form-icon relative mt-2">
+                                            <input name="email" id="email" type="state" class="form-input pl-2"
+                                                placeholder="Enter Your State">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="w-full px-3 sm:w-1/2">
+                                    <div class="text-left">
+                                        <label for="email" class="font-semibold">Enter your PinCode:</label>
+                                        <div class="form-icon relative mt-2">
+                                            <input name="email" id="email" type="pincode"
+                                                class="form-input pl-2" placeholder="Enter Your Pin Code">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="submit" id="submit" name="send"
+                            class="btn bg-indigo-600 w-full hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white rounded-md justify-center flex items-cen ter">Book
+                            Appointment</button>
+                    </form>
+                    <br>
+                </div>
             </div>
         </div>
     </div>
-    <br>
+    <!--end container-->
+    </section>
+    <!--end section-->
+
 </x-guest-layout>
